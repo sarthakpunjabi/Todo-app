9e2642b4ab4bc10c99523d973d2b38b63bf35ec0
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2022-06-24 00:42:15 +0100

    Adding the notes frontend flutter

---
+++ notes_frontend/lib/main.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'note.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const MyHomePage(title: 'Flutter Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  http.Client client = http.Client();
+  List<Note> notes = [];
+
+  @override
+  void initState() {
+    _retrieveNotes();
+    super.initState();
+  }
+
+  _retrieveNotes() async {
+    notes = [];
+
+    List response = json.decode(
+        (await client.get(Uri.parse('http://127.0.0.1:8000/notes/'))).body);
+    response.forEach((element) {
+      notes.add(Note.fromMap(element));
+    });
+    setState(() {});
+  }
+
+  void _addNote() {}
+
+  void _deleteNote(int id) async {
+    await client.delete(Uri.parse('http://127.0.0.1:8000/notes/$id/delete/'));
+    _retrieveNotes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _retrieveNotes();
+        },
+        child: ListView.builder(
+          itemCount: notes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text(notes[index].note),
+              onTap: () {},
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () => _deleteNote(notes[index].id),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addNote,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ notes_frontend/lib/urls.dart
@@ -0,0 +1,9 @@
+// import 'package:http/http.dart';
+
+
+// final url = ;
+// void retrieveUrl() async {
+      
+//     final response = await get(Uri.parse(url));
+
+// }
